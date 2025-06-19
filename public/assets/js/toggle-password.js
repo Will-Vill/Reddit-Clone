db9dd0c0a320e9e32ba0e2af46739148eb6293a2
williamvil1 <VLLWLM03A24C351P@studium.unict.it>
2025-06-19 23:29:31 +0200

    Toggle password condiviso

---
+++ public/assets/js/toggle-password.js
@@ -0,0 +1,19 @@
+function checkPasswordVisibility(event) {
+    const targetInputId = event.currentTarget.dataset.target;
+    const targetInput = document.getElementById(targetInputId);
+    if (targetInput) {
+            if (targetInput.type === 'password') {
+                targetInput.type = 'text';
+                event.currentTarget.textContent = '🙈';
+            } else {
+                targetInput.type = 'password';
+                event.currentTarget.textContent = '👁️';
+            }
+        }
+}
+
+
+const toggle_passwords = document.querySelectorAll('.toggle-password-visibility');
+for(const toggle_password of toggle_passwords) {
+    toggle_password.addEventListener('click',checkPasswordVisibility);
+}
