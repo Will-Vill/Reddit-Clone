fc665c41e8427b47a2fe4ddb9ddfa2ab307f23fa
williamvil1 <VLLWLM03A24C351P@studium.unict.it>
2025-07-01 21:59:04 +0200

    Aggiunti session(success) e session(error)

--- resources/views/post_singolo.blade.php
+++ resources/views/post_singolo.blade.php
@@ -5,6 +5,18 @@
 @section('title', $posts->titolo)
 
 @section('content')
+
+@if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
 <div class="container">
     <div class="contenuto-destro">
         <section class="sezione-main">
