57e6f4febf4b483a116c534de3d9c16dc93c6a32
williamvil1 <VLLWLM03A24C351P@studium.unict.it>
2025-07-01 21:51:01 +0200

    Modificate delle funzioni e semplificata la logica

--- public/assets/js/crea_post.js
+++ public/assets/js/crea_post.js
@@ -7,21 +7,8 @@ if(document.querySelector(".form-creazione-post")) {
 function inizializzaCreaPost() {
     const radioTipoContenuto = document.querySelectorAll('input[name="tipo_contenuto"]');
     
-    // Non serve più il gestore di click sul div, basta quello sul radio
     for (const radio of radioTipoContenuto) {
         radio.addEventListener('change', aggiornaVisibilitaCampiContenuto);
-    }
-    
-    // NON chiamare più la funzione al caricamento, perché Blade ha già fatto il lavoro.
-    // aggiornaVisibilitaCampiContenuto(); 
-}
-
-function handleClick() {
-    var radioButton = this.querySelector('input[type="radio"]');
-    if (radioButton && !radioButton.checked) {
-        radioButton.checked = true;
-        radioButton.dispatchEvent(new Event('change'));
-        console.log('Radio selezionato manualmente:', radioButton.value);
     }
 }
 
@@ -30,7 +17,6 @@ function aggiornaVisibilitaCampiContenuto() {
     const campoTesto = document.getElementById('campo_contenuto_testo');
     const campoImmagine = document.getElementById('campo_contenuto_immagine');
 
-    // La logica ora è più semplice: mostra uno e nascondi l'altro.
     if (tipoSelezionato === 'text') {
         campoTesto.style.display = 'block';
         campoImmagine.style.display = 'none';
