c546ad13ffafafabe4dd9557526d1299df4462e7
William_Villari <villariwilliam7@gmail.com>
2025-07-04 11:44:38 +0200

    Aggiunte traduzioni

---
+++ lang/it/pagination.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'previous' => '« Precedente',
+    'next' => 'Successivo »',
+    'showing' => 'Visualizzazione',
+    'to' => 'a',
+    'of' => 'di',
+    'results' => 'risultati',
+];
+
+?>
